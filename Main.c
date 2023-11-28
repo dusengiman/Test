2c8b063476e295f2356feeeebc45664ebc48078a
Janvier <dusajanvier@gmail.com>
2023-11-28 21:14:37 +0100

    Add_a_b

--- Main.c
+++ Main.c
@@ -4,6 +4,6 @@ int int main(int argc, char const *argv[])
 { int a=2,b=3;
 	printf("Hello word\n");
 	printf("Add numbers\n");
-	printf("d\n",Add_2_Nbrs(2,3));
+	printf("d\n",Add_2_Nbrs(a,b));
 	return 0;
 }
